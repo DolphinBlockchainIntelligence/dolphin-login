dd8fc5bd105e564404ad56e3f0818a08e905363f
Eugen Soloviov <suenot@gmail.com>
2017-10-26 10:53:21 +0300

    turn off analyzer webpack

--- config/index.js
+++ config/index.js
@@ -20,7 +20,8 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     // bundleAnalyzerReport: process.env.npm_config_report
-    bundleAnalyzerReport: true
+    // bundleAnalyzerReport: true
+    bundleAnalyzerReport: false
   },
   dev: {
     env: require('./dev.env'),
